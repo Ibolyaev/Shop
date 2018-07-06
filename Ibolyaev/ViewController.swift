fc699e54b17e87d5e488ac642b191842bdc05890
Igor <mrronin@yandex.ru>
2018-07-06 11:45:01 +0300

    init

---
+++ Ibolyaev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ibolyaev
+//
+//  Created by Ronin on 06/07/2018.
+//  Copyright © 2018 Ronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
